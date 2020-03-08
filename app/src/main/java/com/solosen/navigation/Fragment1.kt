44854c00237f0d9fc35c4cc1635ec7800f8c8c71
Sen Coder <sen.coder9901@gmail.com>
2020-03-08 13:11:19 +0800

    First Commit

---
+++ app/src/main/java/com/solosen/navigation/Fragment1.kt
@@ -0,0 +1,28 @@
+package com.solosen.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment1.*
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_next.setOnClickListener {
+            this.findNavController().navigate(R.id.action_fragment1_to_fragment2)
+        }
+    }
+}
